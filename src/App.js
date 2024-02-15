7a9858c761c10c0d99f1e3277ca6f27f20b2e3e2
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2024-02-15 15:53:41 +0530

    cart-overlay-backprod

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Header from "./Components/Layout/Header";
 import RestaurantMealList from "./Components/Resturant/RestaurantMealList";
+import CartCheckout from "./Components/Cart/CartCheckout";
 function App() {
   return (
     <React.Fragment>
       <div>
+       <CartCheckout></CartCheckout>
+     
         <Header />
       </div>
       <main>
         <RestaurantMealList></RestaurantMealList>
       </main>
+
     </React.Fragment>
   );
 }
