f0b7da3736e241bb794451780e193af07be4a001
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2024-02-15 13:18:35 +0530

    name-correction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import Header from './Components/Layout/Header';
-import RestaurantList from './Components/Resturant/RestaurantList';
+import React from "react";
+import Header from "./Components/Layout/Header";
+import RestaurantMealList from "./Components/Resturant/RestaurantMealList";
 function App() {
-
   return (
     <React.Fragment>
-    <div>
-    <Header />
-    </div>
-    <main>
-    <RestaurantList></RestaurantList>
-    </main>
+      <div>
+        <Header />
+      </div>
+      <main>
+        <RestaurantMealList></RestaurantMealList>
+      </main>
     </React.Fragment>
-  )
+  );
 }
 
 export default App;
