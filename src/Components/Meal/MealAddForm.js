7a4df33badf4b2ab43f8310ff8d924a03ed277ac
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2024-02-20 14:22:24 +0530

    cartAdd/remove/btn

--- src/Components/Meal/MealAddForm.js
+++ src/Components/Meal/MealAddForm.js
@@ -1,13 +1,12 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import classes from "./MealAddForm.module.css";
 
 const MealAddForm = (props) => {
   const amt = useRef();
   const submitFormHandler = (event) => {
     event.preventDefault();
-    
+
     props.getItemAmount(amt.current.value);
-    console.log(amt.current.value);
   };
 
   return (
@@ -15,7 +14,12 @@ const MealAddForm = (props) => {
       <label htmlFor="amount" id={classes.amountLabel}>
         Amount
       </label>
-      <input id={classes.amount} type="number" defaultValue ="1" ref= {amt}></input>
+      <input
+        id={classes.amount}
+        type="number"
+        defaultValue="1"
+        ref={amt}
+      ></input>
       <div>
         <button type="submit" id={classes.Addbtn}>
           + Add
